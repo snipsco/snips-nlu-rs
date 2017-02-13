0d7ed39b5479a1347172ea3b6c516348e1365510
Kevin Lefèvre <kevin.lefevre@snips.ai>
2017-02-13 17:54:58 +0100

    Fix bench compilation

--- queries-core/benches/tokens_classif.rs
+++ queries-core/benches/tokens_classif.rs
@@ -12,36 +12,20 @@ use snips_queries_core::pipeline::tokens_classifier::TokensClassifier;
 use snips_queries_core::preprocess;
 
 fn load_classifier(bench: &mut Bencher) {
-    let cnn_path = path::Path::new("../data/snips-sdk-models-protobuf/tokens_classification/cnn_model_quantized.pb");
-    bench.iter(|| {
-        let mut cnn = TensorflowCNN::new(cnn_path);
-    });
-}
-
-fn load_intent_model(bench: &mut Bencher) {
-    let cnn_path = path::Path::new("../data/snips-sdk-models-protobuf/tokens_classification/cnn_model_quantized.pb");
-    let mut cnn = TensorflowCNN::new(cnn_path);
-
-    let model_directory = "../data/snips-sdk-models-protobuf/tokens_classification/";
-    let model_path = path::Path::new(model_directory)
-        .join("BookRestaurant_CnnCrf")
-        .with_extension("pbbin");
+    let cnn_path = "../data/snips-sdk-models-protobuf/tokens_classification/cnn_model_quantized.pb";
+    let model_path = "../data/snips-sdk-models-protobuf/tokens_classification/";
 
     bench.iter(|| {
-        let mut tokens_classifier = ProtobufTokensClassifier::new(&model_path, &mut cnn);
+        let mut tokens_classifier = ProtobufTokensClassifier::new(&model_path, &cnn_path);
     });
 }
 
 fn run_intent_model(bench: &mut Bencher) {
-    let cnn_path = path::Path::new("../data/snips-sdk-models-protobuf/tokens_classification/cnn_model_quantized.pb");
-    let mut cnn = TensorflowCNN::new(cnn_path);
+    let cnn_path = "../data/snips-sdk-models-protobuf/tokens_classification/cnn_model_quantized.pb";
+    let model_path = "../data/snips-sdk-models-protobuf/tokens_classification/";
 
-    let model_directory = "../data/snips-sdk-models-protobuf/tokens_classification/";
-    let model_path = path::Path::new(model_directory)
-        .join("BookRestaurant_CnnCrf")
-        .with_extension("pbbin");
+    let mut tokens_classifier = ProtobufTokensClassifier::new(&model_path, &cnn_path);
 
-    let mut tokens_classifier = ProtobufTokensClassifier::new(&model_path, &mut cnn);
     let preprocessor_result = preprocess("Book me a table for two people at Le Chalet Savoyard");
 
     bench.iter(|| {
@@ -49,5 +33,5 @@ fn run_intent_model(bench: &mut Bencher) {
     });
 }
 
-benchmark_group!(tokens_classifier, load_classifier, load_intent_model, run_intent_model);
+benchmark_group!(tokens_classifier, load_classifier, run_intent_model);
 benchmark_main!(tokens_classifier);
