93296bf78a60668c0e2d500fba1f9af55b7d9e3c
Thibaut Lorrain <thibaut.lorrain@snips.ai>
2017-03-14 10:48:47 +0100

    review

--- queries-core/benches/intent_parser.rs
+++ queries-core/benches/intent_parser.rs
@@ -3,6 +3,9 @@ extern crate bencher;
 
 extern crate queries_core;
 
+extern crate yolo;
+use yolo::Yolo;
+
 use bencher::Bencher;
 use queries_core::FileConfiguration;
 use queries_core::IntentParser;
@@ -11,13 +14,13 @@ fn load_intent_parser(bench: &mut Bencher) {
     let file_configuration = FileConfiguration::default();
 
     bench.iter(|| {
-        let _ = IntentParser::new(&file_configuration, Some(&["BookRestaurant"])).unwrap();
+        let _ = IntentParser::new(&file_configuration, Some(&["BookRestaurant"])).yolo();
     });
 }
 
 fn run_intent_classifications(bench: &mut Bencher) {
     let file_configuration = FileConfiguration::default();
-    let intent_parser = IntentParser::new(&file_configuration, Some(&["BookRestaurant"])).unwrap();
+    let intent_parser = IntentParser::new(&file_configuration, Some(&["BookRestaurant"])).yolo();
 
     bench.iter(|| {
         let _ = intent_parser.run_intent_classifiers("Book me a restaurant for two peoples at Le Chalet Savoyard", 0.4, None);
@@ -28,7 +31,7 @@ fn run_everything(bench: &mut Bencher) {
     let file_configuration = FileConfiguration::default();
     
     bench.iter(|| {
-        let intent_parser = IntentParser::new(&file_configuration, Some(&["BookRestaurant"])).unwrap();
+        let intent_parser = IntentParser::new(&file_configuration, Some(&["BookRestaurant"])).yolo();
         let text = "Book me a restaurant for two peoples at Le Chalet Savoyard";
         let result = intent_parser.run_intent_classifiers(text, 0.4, None);
         let _ = intent_parser.run_tokens_classifier(text, &result[0].name);
