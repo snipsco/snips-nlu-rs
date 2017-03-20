649ef995b2d4905eb37474f90b124c3a458c877e
Tristan Deleu <tristan.deleu@gmail.com>
2017-03-20 18:12:56 +0100

    Implement Classifier trait for TensorFlowClassifier

--- queries-core/src/models/tf.rs
+++ queries-core/src/models/tf.rs
@@ -1,5 +1,7 @@
 use std::{ fs, path, sync };
 use std::io::Read;
+use std::f32;
+use std::cmp;
 
 use errors::*;
 use ndarray::prelude::*;
@@ -11,24 +13,31 @@ use tensorflow::SessionOptions;
 use tensorflow::StepWithGraph;
 use tensorflow::Tensor;
 
-use pipeline::Probability;
-
-
 pub struct TensorFlowClassifier(sync::Mutex<(Session, Graph)>);
 pub struct TensorFlowCRFClassifier(TensorFlowClassifier);
 
 pub trait Classifier {
     fn predict_proba(&self, features: &Array2<f32>) -> Result<Array2<f32>>;
-    fn predict(&self, features: &Array2<f32>) -> Result<Array1<u32>>;
+    fn predict(&self, features: &Array2<f32>) -> Result<Array1<usize>>;
 }
 
 fn array_to_tensor(array: &Array2<f32>) -> Result<Tensor<f32>> {
     let shape = array.shape();
-    let mut tensor: Tensor<f32> = Tensor::new(&[array.shape()[0] as u64, array.shape()[1] as u64]);
-    for (&mut v1, &v2) in tensor.iter_mut().zip(array.view().iter()) {
-        v1 = v2
+    let mut tensor: Tensor<f32> = Tensor::new(&[shape[0] as u64, shape[1] as u64]);
+    for (i, &elem) in array.view().iter().enumerate() {
+        tensor[i] = elem
     }
+
     Ok(tensor)
+}
+
+fn tensor_to_array(tensor: &Tensor<f32>) -> Result<Array2<f32>> {
+    let shape = tensor.dims();
+    let mut vec = Vec::with_capacity(tensor.len());
+    vec.extend_from_slice(&tensor.data());
+    let array = Array::from_vec(vec).into_shape((shape[0] as usize, shape[1] as usize))?;
+
+    Ok(array)
 }
 
 impl TensorFlowClassifier {
@@ -49,20 +58,68 @@ impl TensorFlowClassifier {
         let result: Result<Tensor<f32>> = {
             let mut locked = self.0.lock().map_err(|_| "Can not take lock on TensorFlow. Mutex poisoned")?;
             let (ref mut session, ref graph) = *locked;
-            Ok(Tensor::<f32>::new(&[2 as u64, 2 as u64]))
+            step.add_input(&graph.operation_by_name_required("input")?, 0, &x);
+            let res = step.request_output(&graph.operation_by_name_required("logits")?, 0);
+
+            session.run(&mut step)?;
+
+            Ok(step.take_output(res)?)
         };
-        Ok(Array2::<f32>::zeros((1, 1)))
+        
+        tensor_to_array(&(result?))
+    }
+}
+
+impl Classifier for TensorFlowClassifier {
+    fn predict_proba(&self, features: &Array2<f32>) -> Result<Array2<f32>> {
+        let mut logits = self.run(features)?;
+        for mut row in logits.outer_iter_mut() {
+            let max = *(row.iter().max_by(|a, b| a.partial_cmp(b).unwrap_or(cmp::Ordering::Less)).unwrap());
+            for value in row.iter_mut() {
+                *value = (*value - max).exp()
+            }
+            let sum_exponent: f32 = row.iter().sum();
+            row /= sum_exponent;
+        }
+
+        Ok(logits)
+    }
+
+    fn predict(&self, features: &Array2<f32>) -> Result<Array1<usize>> {
+        let logits = self.run(features)?;
+        let predictions: Array1<usize> = Array::from_iter(logits.outer_iter().map(|row| {
+            let mut index = 0;
+            let mut max_value = f32::NEG_INFINITY;
+            for (j, &value) in row.iter().enumerate() {
+                if value > max_value {
+                    index = j;
+                    max_value = value;
+                }
+            }
+
+            index
+        }));
+
+        Ok(predictions)
     }
 }
 
 #[cfg(test)]
 mod test {
     use std::path::Path;
-    use models::tf::{ TensorFlowClassifier };
+    use ndarray::prelude::*;
+
+    use models::tf::{ TensorFlowClassifier, Classifier };
 
     #[test]
     fn tf_classifier_works() {
         let model_path = Path::new("../data/snips-sdk-models-protobuf/models/tokens_classification/BookRestaurant_model.pb");
         let model = TensorFlowClassifier::new(model_path).unwrap();
+        let features = Array2::<f32>::zeros((11, 1358));
+        let probas = model.predict_proba(&features);
+        let predictions = model.predict(&features);
+
+        println!("probas: {:?}", probas);
+        println!("predictions: {:?}", predictions);
     }
 }
