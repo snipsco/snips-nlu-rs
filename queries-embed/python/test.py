97d0cec2bb509ace527b52c332c1c33f72691ec9
Kevin Lefèvre <kevin.lefevre@snips.ai>
2017-03-29 14:06:06 +0200

    Fix IntentParser and add duckling

--- queries-embed/python/test.py
+++ queries-embed/python/test.py
@@ -1,10 +1,18 @@
+# coding=utf-8
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
 from snips_queries import IntentParser
 
-#q=IntentParser(data_binary=bytearray(open("/home/fredszaq/Work/tmp/builtins_final.zip", "rb").read()))
-q=IntentParser(data_path="/home/fredszaq/Work/tmp/builtins_final")
+q = IntentParser("en", data_path="../../data/untracked")
+#q = IntentParser("en", data_binary=bytearray(open("../../data/untracked/builtins_final.zip", "rb").read()))
 
-r= q.get_intent("book me a restaurant for five people tonight")
-print("%s" % r) 
+text = "Book me a restaurant for five people tonight"
 
-r= q.get_entities("book me a restaurant for five people tonight", "BookRestaurant")
-print("%s" % r) 
+r = q.get_intent(text, 0.4)
+print("%s", r)
+
+r = q.get_entities(text, "BookRestaurant")
+print("%s", r)
