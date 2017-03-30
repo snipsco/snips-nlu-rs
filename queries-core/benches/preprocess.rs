3e7eb7fca2d76cf2aa43428cf2af5047ed178db7
Kevin Lefèvre <kevin.lefevre@snips.ai>
2017-03-30 12:10:18 +0200

    Fix bench

--- queries-core/benches/preprocess.rs
+++ queries-core/benches/preprocess.rs
@@ -1,30 +1,29 @@
 #[macro_use]
 extern crate bencher;
-
 extern crate queries_core;
 
 use bencher::Bencher;
-use queries_core::preprocess;
 
 fn prep_short(bench: &mut Bencher) {
-    bench.iter(|| { preprocess("Shut up"); })
+    bench.iter(|| { queries_core::preprocess("Shut up"); })
 }
 
 fn prep_medium(bench: &mut Bencher) {
-    bench.iter(|| preprocess("Book me a uber right now"))
+    bench.iter(|| queries_core::preprocess("Book me a uber right now"))
 }
 
 fn prep_long(bench: &mut Bencher) {
     bench.iter(|| {
-        preprocess("Book me a table for four people at a Café nearby for tomorrow night at 8pm")
+        queries_core::preprocess("Book me a table for four people at a Café nearby for tomorrow night at 8pm")
     })
 }
 
 fn prep_longer(bench: &mut Bencher) {
     bench.iter(|| {
-        preprocess("Book me a table for four people at a Café nearby for tomorrow night at 8pm \
-                    that my parents will like because they have terrible taste and don't like \
-                    exotic food because they fear it will be too spicy")
+        queries_core::preprocess(
+            "Book me a table for four people at a Café nearby for tomorrow night at 8pm \
+            that my parents will like because they have terrible taste and don't like \
+            exotic food because they fear it will be too spicy")
     })
 }
 
