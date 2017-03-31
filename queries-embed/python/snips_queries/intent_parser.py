5ffe0253997169f5b28fe710cc26af12f6bffc28
Kevin Lefèvre <kevin.lefevre@snips.ai>
2017-03-31 10:32:47 +0200

    Ran autopep8

--- queries-embed/python/snips_queries/intent_parser.py
+++ queries-embed/python/snips_queries/intent_parser.py
@@ -14,7 +14,7 @@ lib = cdll.LoadLibrary(os.path.join(os.path.dirname(__file__), "../queries_embed
 
 class IntentParser(object):
 
-    def __init__(self, language, data_path = None, data_binary = None):
+    def __init__(self, language, data_path=None, data_binary=None):
         self.language = language
 
         if data_path is None and data_binary is None:
@@ -23,16 +23,18 @@ class IntentParser(object):
         if data_path is not None:
             self.data_path = data_path
             self._parser = pointer(c_void_p())
-            exit_code = lib.intent_parser_create_from_dir(data_path.encode("utf-8"), byref(self._parser))
+            exit_code = lib.intent_parser_create_from_dir(
+                data_path.encode("utf-8"), byref(self._parser))
 
         if data_binary is not None:
             self._parser = pointer(c_void_p())
             bytearray_type = c_char * len(data_binary)
-            exit_code = lib.intent_parser_create_from_binary(bytearray_type.from_buffer(data_binary), len(data_binary), byref(self._parser))
+            exit_code = lib.intent_parser_create_from_binary(
+                bytearray_type.from_buffer(data_binary), len(data_binary), byref(self._parser))
 
         if exit_code != 1:
             raise ImportError('Something wrong happened while creating the '
-                'intent parser. See stderr.')
+                              'intent parser. See stderr.')
 
         duckling.load()
 
@@ -44,11 +46,11 @@ class IntentParser(object):
 
         pointer = c_char_p()
         lib.intent_parser_run_intent_classification(
-                self._parser,
-                query.encode("utf-8"),
-                c_float(threshold),
-                json.dumps(entities),
-                byref(pointer))
+            self._parser,
+            query.encode("utf-8"),
+            c_float(threshold),
+            json.dumps(entities),
+            byref(pointer))
         result = string_at(pointer)
         lib.intent_parser_destroy_string(pointer)
 
@@ -59,18 +61,24 @@ class IntentParser(object):
 
         pointer = c_char_p()
         lib.intent_parser_run_tokens_classification(
-                self._parser,
-                query.encode("utf-8"),
-                intent.encode("utf-8"),
-                json.dumps(entities),
-                byref(pointer))
+            self._parser,
+            query.encode("utf-8"),
+            intent.encode("utf-8"),
+            json.dumps(entities),
+            byref(pointer))
         result = string_at(pointer)
         lib.intent_parser_destroy_string(pointer)
 
         return json.loads(result)
 
     def _build_entities(self, query):
-        tokens = duckling.parse(self.language, query, ["number", "time", "duration", "ordinal", "temperature", "amount-of-money"])
+        tokens = duckling.parse(self.language, query, [
+                                "number",
+                                "time",
+                                "duration",
+                                "ordinal",
+                                "temperature",
+                                "amount-of-money"])
         symbol_mapping = {
             "time": "%TIME%",
             "number": "%NUMBER%",
