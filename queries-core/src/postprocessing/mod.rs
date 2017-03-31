7195ad5591949707821ccb9d381e77b4ae162377
Kevin Lefèvre <kevin.lefevre@snips.ai>
2017-03-31 15:04:42 +0200

    Use `mapv_into`

--- queries-core/src/postprocessing/mod.rs
+++ queries-core/src/postprocessing/mod.rs
@@ -1,8 +1,5 @@
 use ndarray::prelude::*;
 
 pub fn bilou(predictions: Array1<usize>) -> Array1<usize> {
-    predictions
-        .into_iter()
-        .map(|p| (p + 3) / 4)
-        .collect()
+    predictions.mapv_into(|p| (p + 3) / 4)
 }
