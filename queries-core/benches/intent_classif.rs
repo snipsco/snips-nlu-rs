36bfab67f1edfd278bd9e08829e5552efa0d03a0
Kevin Lefèvre <kevin.lefevre@snips.ai>
2017-04-03 23:10:08 +0200

    Rename benches

--- queries-core/benches/intent_classif.rs
+++ queries-core/benches/intent_classif.rs
@@ -9,7 +9,7 @@ use yolo::Yolo;
 use queries_core::config::{AssistantConfig, FileBasedAssistantConfig};
 use queries_core::pipeline::intent_classifier::{IntentClassifier, ProtobufIntentClassifier};
 
-fn get_classifier(classifier: &str) -> ProtobufIntentClassifier {
+fn get_intent_classifier(classifier: &str) -> ProtobufIntentClassifier {
     let root_dir = queries_core::file_path("untracked");
     let assistant_config = FileBasedAssistantConfig::new(root_dir);
     let intent_config = assistant_config
@@ -18,14 +18,14 @@ fn get_classifier(classifier: &str) -> ProtobufIntentClassifier {
     ProtobufIntentClassifier::new(intent_config).yolo()
 }
 
-fn load_classifier(bench: &mut Bencher) {
+fn load_intent_classifier(bench: &mut Bencher) {
     bench.iter(|| {
-        let _ = get_classifier("BookRestaurant");
+        let _ = get_intent_classifier("BookRestaurant");
     });
 }
 
-fn run_classifier(bench: &mut Bencher) {
-    let classifier = get_classifier("BookRestaurant");
+fn run_intent_classifier(bench: &mut Bencher) {
+    let classifier = get_intent_classifier("BookRestaurant");
     let preprocessor_result = queries_core::preprocess(
         "Book me a table for four people at Le Chalet Savoyard tonight",
         r#"[{"end_index": 24, "value": "four", "start_index": 20, "entity": "%NUMBER%"}, {"end_index": 61, "value": "tonight", "start_index": 54, "entity": "%TIME_INTERVAL%"}]"#)
@@ -34,5 +34,5 @@ fn run_classifier(bench: &mut Bencher) {
     bench.iter(|| { let _ = classifier.run(&preprocessor_result); });
 }
 
-benchmark_group!(intent_classifier, load_classifier, run_classifier);
+benchmark_group!(intent_classifier, load_intent_classifier, run_intent_classifier);
 benchmark_main!(intent_classifier);
