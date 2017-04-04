e4f612d4c95b27aa8f8bc060c230e0fe287b55bb
Kevin Lefèvre <kevin.lefevre@snips.ai>
2017-04-04 17:24:55 +0200

    Sort alphabetically

--- queries-core/src/pipeline/mod.rs
+++ queries-core/src/pipeline/mod.rs
@@ -1,9 +1,9 @@
 mod intent_configuration;
 pub mod feature_processor;
-pub mod slot_filler;
 pub mod intent_classifier;
-pub mod tokens_classifier;
 pub mod intent_parser;
+pub mod slot_filler;
+pub mod tokens_classifier;
 
 pub type Probability = f32;
 
