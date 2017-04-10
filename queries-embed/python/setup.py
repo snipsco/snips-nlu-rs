d2e29c75b98bcba5f8d4daf6bd0325eb4770f906
Kevin Lefèvre <kevin.lefevre@snips.ai>
2017-04-10 11:34:57 +0200

    Bump duckling

--- queries-embed/python/setup.py
+++ queries-embed/python/setup.py
@@ -25,7 +25,7 @@ setup(
     author_email='thibaut.lorrain@snips.ai',
     packages=["snips_queries"],
     install_requires=[
-        "duckling==0.0.12",
+        "duckling==0.0.14",
     ],
     cmdclass={
         'build_rust': build_rust_cmdclass(debug=False),
