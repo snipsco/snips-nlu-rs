eb091b8305bca2b711da328a3a4720cd561fed53
Thibaut Lorrain <thibaut.lorrain@snips.ai>
2017-04-14 17:14:14 +0200

    implementation of FstGazetteer

--- queries-core/src/models/gazetteer.rs
+++ queries-core/src/models/gazetteer.rs
@@ -1,8 +1,12 @@
+use std::collections::{HashSet, HashMap};
 use std::io::prelude::*;
-use std::collections::HashSet;
 use std::iter::FromIterator;
+use std::path::Path;
+use std::sync::Arc;
 
 use errors::*;
+use csv;
+use fst;
 use serde_json;
 
 pub trait Gazetteer {
@@ -14,7 +18,7 @@ pub struct HashSetGazetteer {
 }
 
 impl HashSetGazetteer {
-    pub fn new(r: &mut Read) -> Result<HashSetGazetteer> {
+    pub fn new(r: &mut Read) -> Result<Self> {
         let vec: Vec<String> = serde_json::from_reader(r)?;
         Ok(HashSetGazetteer { values: HashSet::from_iter(vec) })
     }
@@ -23,6 +27,86 @@ impl HashSetGazetteer {
 impl Gazetteer for HashSetGazetteer {
     fn contains(&self, value: &str) -> bool {
         self.values.contains(value)
+    }
+}
+
+#[derive(Debug, Hash, PartialEq, Eq)]
+pub struct GazetteerKey {
+    pub lang: String,
+    pub category: String,
+    pub name: String,
+    pub version: String,
+}
+
+type Header = Arc<HashMap<GazetteerKey, Arc<HashSet<u64>>>>;
+
+#[derive(Clone)]
+pub struct FstGazetteerFactory {
+    map: Arc<fst::Map>,
+    header: Header
+}
+
+impl FstGazetteerFactory {
+    pub fn new_mmap<F: AsRef<Path>, H: Read>(fst_path: F, header_reader: &mut H) -> Result<Self> {
+        Ok(FstGazetteerFactory {
+            map: Arc::new(fst::Map::from_path(fst_path)?),
+            header: FstGazetteerFactory::build_header(header_reader)?
+        })
+    }
+
+    pub fn new_ram<F: Read, H: Read>(fst_reader: &mut F, header_reader: &mut H) -> Result<Self> {
+        let mut fst_bytes: Vec<u8> = vec![];
+        fst_reader.read_to_end(&mut fst_bytes)?;
+        Ok(FstGazetteerFactory {
+            map: Arc::new(fst::Map::from_bytes(fst_bytes)?),
+            header: FstGazetteerFactory::build_header(header_reader)?
+        })
+    }
+
+    fn build_header<R: Read>(reader: &mut R) -> Result<Header> {
+        let mut csv_reader = csv::Reader::from_reader(reader).delimiter(b':').has_headers(false);
+        let mut header = HashMap::new();
+
+        for row in csv_reader.decode() {
+            let (lang, cat, name, version, ids): (String, String, String, String, String) = row?;
+            let id_set = ids.split(",").map(|it| it.parse().unwrap()).collect::<HashSet<u64>>();
+            header.insert(
+                GazetteerKey {
+                    lang: lang,
+                    category: cat,
+                    name: name,
+                    version: version
+                },
+                Arc::new(id_set));
+        }
+
+        return Ok(Arc::new(header))
+    }
+
+    pub fn get_gazetteer(&self, key: &GazetteerKey) -> Result<Box<Gazetteer>> {
+        if let Some(possible_values) = self.header.get(key) {
+            Ok(Box::new(FstGazetteer {
+                map: self.map.clone(),
+                possible_values: possible_values.clone(),
+            }))
+        } else {
+            bail!("Could not find gazetteer {:?}", key)
+        }
+    }
+}
+
+pub struct FstGazetteer {
+    map: Arc<fst::Map>,
+    possible_values: Arc<HashSet<u64>>
+}
+
+impl Gazetteer for FstGazetteer {
+    fn contains(&self, value: &str) -> bool {
+        if let Some(v) = self.map.get(value) {
+            self.possible_values.contains(&v)
+        } else {
+            false
+        }
     }
 }
 
