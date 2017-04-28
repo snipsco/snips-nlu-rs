8d53852cffc1b9f313cd3b64a117b45ad5ec2c62
Kevin Lefèvre <kevin.lefevre@snips.ai>
2017-04-28 14:55:32 +0200

    Use language of intents and add support for multiple languages

--- queries-core/src/pipeline/deep/intent_configuration.rs
+++ queries-core/src/pipeline/deep/intent_configuration.rs
@@ -1,12 +1,15 @@
+use std::str::FromStr;
+
 use ndarray::prelude::*;
 
 use errors::*;
-
+use preprocessing::Lang;
 use pipeline::{Probability, Prediction};
 use super::tf_classifier_wrapper::TFClassifierWrapper;
 use config::ArcBoxedIntentConfig;
 
 pub struct IntentConfiguration {
+    pub language: Lang,
     pub intent_classifier: TFClassifierWrapper<Probability>,
     pub tokens_classifier: TFClassifierWrapper<Array1<Prediction>>,
     pub slot_names: Vec<String>,
@@ -17,8 +20,10 @@ impl IntentConfiguration {
     pub fn new(intent_config: ArcBoxedIntentConfig) -> Result<IntentConfiguration> {
         let data = intent_config.get_pb_config()?;
         let slots = data.get_slots().iter().map(|s| s.get_name().to_string()).collect();
+        let lang = Lang::from_str(data.get_language()).map_err(|e| format!("language not supported: {}", e))?;
 
         Ok(IntentConfiguration {
+            language: lang,
             intent_classifier: TFClassifierWrapper::new_intent_classifier(intent_config.clone())?,
             tokens_classifier: TFClassifierWrapper::new_tokens_classifier(intent_config.clone())?,
             intent_name: data.name.clone(),
