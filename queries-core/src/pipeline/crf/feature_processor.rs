a76c1d15c34f2ee91881920a956e5453464bd5c5
Thibaut Lorrain <thibaut.lorrain@snips.ai>
2017-05-09 18:01:48 +0200

    clean up offsets and prevent key recomputation in CRF feature processor

--- queries-core/src/pipeline/crf/feature_processor.rs
+++ queries-core/src/pipeline/crf/feature_processor.rs
@@ -9,8 +9,21 @@ use protos::feature::{Feature, Feature_Vector_Type};
 use super::features;
 
 struct FeatureFunction {
-    function: Box<Fn(&Vec<Token>, usize) -> Option<(String, String)>>,
-    offsets: Option<Vec<i32>>
+    function: Box<Fn(&[Token], usize) -> Option<String>>,
+    offsets: Vec<(i32, String)>,
+}
+
+
+impl FeatureFunction {
+    fn new<T: 'static>(key: String, offsets: Vec<i32>, function: T) -> FeatureFunction
+        where T: Fn(&[Token], usize) -> Option<String> {
+        let offsets = offsets
+            .into_iter()
+            .map(|i| (i, if i == 0 { key.clone() } else { format!("{}[{:+}]", key, i) }))
+            .collect();
+        let function = Box::new(function);
+        FeatureFunction { offsets, function }
+    }
 }
 
 struct CrfFeatureProcessor {
@@ -21,26 +34,17 @@ impl FeatureProcessor<Vec<Token>, Vec<Vec<(String, String)>>> for CrfFeatureProc
     fn compute_features(&self, input: &Vec<Token>) -> Vec<Vec<(String, String)>> {
         self.functions.iter().fold(vec![vec![]; input.len()], |mut acc, f| {
             (0..input.len()).foreach(|i| {
-                if let Some(kv) = (f.function)(input, i) {
-                    if let Some(ref offsets) = f.offsets {
-                        offsets.iter().foreach(|offset| {
-                            if *offset != 0
-                                && i as i32 - offset >= 0
-                                && i as i32 - offset < input.len() as i32 {
-                                acc[(i as i32 - offset) as usize].push(offset_key(&kv, *offset));
-                            }
-                        })
-                    }
-                    acc[i].push(kv)
+                if let Some(value) = (f.function)(input, i) {
+                    f.offsets.iter().foreach(|&(offset, ref key)| {
+                        if i as i32 - offset >= 0 && i as i32 - offset < input.len() as i32 {
+                            acc[(i as i32 - offset) as usize].push((key.clone(), value.clone()));
+                        }
+                    })
                 }
             });
             acc
         })
     }
-}
-
-fn offset_key(&(ref key, ref value): &(String, String), offset: i32) -> (String, String) {
-    (format!("{}[{:+}]", key, offset), value.clone())
 }
 
 impl CrfFeatureProcessor {
@@ -52,17 +56,17 @@ impl CrfFeatureProcessor {
 }
 
 fn get_feature_function(f: &Feature) -> FeatureFunction {
-    let function: Box<Fn(&Vec<Token>, usize) -> Option<(String, String)>> =
-        match f.get_vector_type() {
-            // TODO use proper type from protobuf
-            Feature_Vector_Type::IS_FIRST_WORD => Box::new(|_, i| features::is_first(i)),
-            Feature_Vector_Type::IS_LAST_WORD => Box::new(|t, i| features::is_last(t, i)), // TODO feature key should not be computed each time
-            _ => panic!()
-        };
-
-    let offsets = None; // TODO get that from protobuf when added
-
-    FeatureFunction { function, offsets }
+    let offsets = vec![0]; // TODO read this from protobuf when added
+    match f.get_vector_type() {
+        // TODO use proper type from protobuf
+        Feature_Vector_Type::IS_FIRST_WORD => FeatureFunction::new("is_first".to_string(), offsets, |_, i| features::is_first(i)),
+        Feature_Vector_Type::IS_LAST_WORD => FeatureFunction::new("is_last".to_string(), offsets, |t, i| features::is_last(t, i)),
+        Feature_Vector_Type::NGRAM_MATCHER => {
+            let n = f.get_arguments()[0].get_scalar() as usize; // TODO use proper arg + type
+            FeatureFunction::new(format!("ngram_{}", n), offsets, move |t, i| features::ngram(t, i, n))
+        }
+        _ => panic!()
+    }
 }
 
 
@@ -77,14 +81,12 @@ mod tests {
     #[test]
     fn compute_features_works() {
         let fp = CrfFeatureProcessor {
-            functions: vec![FeatureFunction {
-                function: Box::new(|_, i|
-                    if i == 0 { None } else {
-                        Some(("Toto".to_string(), "Foobar".to_string()))
-                    }
-                ),
-                offsets: None
-            }]
+            functions: vec![FeatureFunction::new("Toto".to_string(), vec![0],
+                                                 |_, i|
+                                                     if i == 0 { None } else {
+                                                         Some("Foobar".to_string())
+                                                     }
+                                                 )]
         };
 
         let computed_features = fp.compute_features(&tokenize("hello world how are you ?"));
@@ -100,30 +102,34 @@ mod tests {
     #[test]
     fn offset_works() {
         let fp = CrfFeatureProcessor {
-            functions: vec![FeatureFunction {
-                function: Box::new(|x, i|
-                    if i == 0 { None } else {
-                        Some(("Toto".to_string(), x[i].value.clone()))
-                    }
-                ),
-                offsets: Some(vec![-2, 0, 2, 4])
-            }]
+            functions: vec![FeatureFunction::new("Toto".to_string(), vec![-2, 0, 2, 4],
+                                                 |x, i|
+                                                     if i == 0 { None } else {
+                                                         Some(x[i].value.clone())
+                                                     }
+                                                 ),
+                            FeatureFunction::new("Tutu".to_string(), vec![2],
+                                                 |_, i|
+                                                     if i != 3 { None } else {
+                                                         Some("Foobar".to_string())
+                                                     }
+                                                 )]
         };
 
         let computed_features = fp.compute_features(&tokenize("hello world how are you ?"));
-
 
         assert_eq!(computed_features, vec![
             vec![("Toto[+2]".to_string(), "how".to_string()),
                  ("Toto[+4]".to_string(), "you".to_string())],
             vec![("Toto".to_string(), "world".to_string()),
-                 ("Toto[+2]".to_string(), "are".to_string()), ],
+                 ("Toto[+2]".to_string(), "are".to_string()),
+                 ("Tutu[+2]".to_string(), "Foobar".to_string())],
             vec![("Toto".to_string(), "how".to_string()),
-                 ("Toto[+2]".to_string(), "you".to_string()), ],
+                 ("Toto[+2]".to_string(), "you".to_string())],
             vec![("Toto[-2]".to_string(), "world".to_string()),
-                 ("Toto".to_string(), "are".to_string()), ],
+                 ("Toto".to_string(), "are".to_string())],
             vec![("Toto[-2]".to_string(), "how".to_string()),
-                 ("Toto".to_string(), "you".to_string()), ],
+                 ("Toto".to_string(), "you".to_string())],
         ]);
     }
 }
--- queries-core/src/pipeline/crf/features.rs
+++ queries-core/src/pipeline/crf/features.rs
@@ -3,19 +3,18 @@ use itertools::Itertools;
 use preprocessing::Token;
 
 
-pub fn is_first(i: usize) -> Option<(String, String)> {
-    if i == 0 { Some(("is_first".to_string(), "1".to_string())) } else { None }
+pub fn is_first(i: usize) -> Option<String> {
+    if i == 0 { Some("1".to_string()) } else { None }
 }
 
-pub fn is_last(t: &[Token], i: usize) -> Option<(String, String)> {
-    if i == t.len() - 1 { Some(("is_last".to_string(), "1".to_string())) } else { None }
+pub fn is_last(t: &[Token], i: usize) -> Option<String> {
+    if i == t.len() - 1 { Some("1".to_string()) } else { None }
 }
 
-pub fn ngram(t: &[Token], i: usize, n: usize) -> Option<(String, String)> {
+pub fn ngram(t: &[Token], i: usize, n: usize) -> Option<String> {
     // TODO we should precompute the ascii lowercase value somewhere, perhaps use NormalizedToken ?
     if i + n > t.len() { None } else {
-        Some((format!("ngram_{}", n),
-              t[i..i + n].iter().map(|t| t.value.to_ascii_lowercase()).join(" ")))
+        Some(t[i..i + n].iter().map(|t| t.value.to_ascii_lowercase()).join(" "))
     }
 }
 
@@ -31,16 +30,16 @@ mod tests {
         let tokens = tokenize("I love house music");
 
         let expected_ngrams = vec![
-            vec![Some(("ngram_1".to_string(), "i".to_string())),
-                 Some(("ngram_1".to_string(), "love".to_string())),
-                 Some(("ngram_1".to_string(), "house".to_string())),
-                 Some(("ngram_1".to_string(), "music".to_string()))],
-            vec![Some(("ngram_2".to_string(), "i love".to_string())),
-                 Some(("ngram_2".to_string(), "love house".to_string())),
-                 Some(("ngram_2".to_string(), "house music".to_string())),
+            vec![Some("i".to_string()),
+                 Some("love".to_string()),
+                 Some("house".to_string()),
+                 Some("music".to_string())],
+            vec![Some("i love".to_string()),
+                 Some("love house".to_string()),
+                 Some("house music".to_string()),
                  None],
-            vec![Some(("ngram_3".to_string(), "i love house".to_string())),
-                 Some(("ngram_3".to_string(), "love house music".to_string())),
+            vec![Some("i love house".to_string()),
+                 Some("love house music".to_string()),
                  None,
                  None]
         ];
