2d15b33af4261641174423482533cc4f392e589b
Kevin Lefèvre <kevin.lefevre@snips.ai>
2017-05-22 15:40:26 +0200

    Use SnipsIntentParser everywhere

--- queries-core/src/pipeline/deep/mod.rs
+++ queries-core/src/pipeline/deep/mod.rs
@@ -1,7 +1,7 @@
 mod feature_processor;
 pub mod tf_classifier_wrapper;
 mod intent_configuration;
-pub mod intent_parser;
+mod intent_parser;
 mod slot_filler;
 
-pub use self::intent_parser::DeepIntentParser as IntentParser;
+pub use self::intent_parser::DeepIntentParser;
--- queries-core/src/pipeline/probabilistic/mod.rs
+++ queries-core/src/pipeline/probabilistic/mod.rs
@@ -1,3 +1,5 @@
 mod features;
-pub mod configuration;
-pub mod feature_processor;
+mod configuration;
+mod feature_processor;
+
+pub use self::configuration::ProbabilisticParserConfiguration;
