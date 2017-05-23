8974939c6e671266922a1ebae1b882e27be4a072
Kevin Lefèvre <kevin.lefevre@snips.ai>
2017-05-23 11:43:06 +0200

    Made FeatureFunction and Tagger Send and Sync

--- queries-core/src/pipeline/probabilistic/tagger.rs
+++ queries-core/src/pipeline/probabilistic/tagger.rs
@@ -1,3 +1,5 @@
+use std::sync;
+
 use crfsuite::Tagger as CrfTagger;
 
 use errors::*;
@@ -8,22 +10,18 @@ use super::crf_utils::TaggingScheme;
 
 pub struct Tagger {
     pub tagging_scheme: TaggingScheme,
-    tagger: CrfTagger,
+    tagger: sync::Mutex<CrfTagger>,
     feature_processor: ProbabilisticFeatureProcessor,
 }
-
-unsafe impl Send for Tagger {}
-unsafe impl Sync for Tagger {}
 
 impl Tagger {
     pub fn new(crf_data: &[u8], tagging_scheme: TaggingScheme, feature_processor: ProbabilisticFeatureProcessor) -> Result<Tagger> {
         let tagger = CrfTagger::create_from_memory(crf_data)?;
-        Ok(Tagger { tagger, tagging_scheme, feature_processor })
+        Ok(Tagger { tagger: sync::Mutex::new(tagger), tagging_scheme, feature_processor })
     }
 
     pub fn get_tags(&self, tokens: &[Token]) -> Result<Vec<String>> {
         let features = self.feature_processor.compute_features(&tokens);
-
-        Ok(self.tagger.tag(&features)?)
+        Ok(self.tagger.lock()?.tag(&features)?)
     }
 }
