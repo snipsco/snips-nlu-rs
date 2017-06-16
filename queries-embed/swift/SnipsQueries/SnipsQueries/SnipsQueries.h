2098b340bc34999ac3c65876dcdbd9bb52bf3303
Kevin Lefevre <kevin.lefevre@snips.ai>
2017-06-16 18:47:20 +0200

    Add iOS and macOS support

---
+++ queries-embed/swift/SnipsQueries/SnipsQueries/SnipsQueries.h
@@ -0,0 +1,19 @@
+//
+//  SnipsQueries.h
+//  SnipsQueries
+//
+//  Created by Kevin Lefevre on 23/06/2017.
+//  Copyright © 2017 Snips. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SnipsQueries.
+FOUNDATION_EXPORT double SnipsQueriesVersionNumber;
+
+//! Project version string for SnipsQueries.
+FOUNDATION_EXPORT const unsigned char SnipsQueriesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SnipsQueries/PublicHeader.h>
+
+
