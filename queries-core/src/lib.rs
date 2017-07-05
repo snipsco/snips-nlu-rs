fdafaf5518ec256dd4f6f5d013220683ba483172
Kevin Lefevre <kevin.lefevre@snips.ai>
2017-07-05 17:54:47 +0200

    Add deprecation message

--- queries-core/src/lib.rs
+++ queries-core/src/lib.rs
@@ -34,6 +34,8 @@ pub use pipeline::assistant_config::{FileBasedConfiguration,
                                      ZipBasedConfiguration};
 pub use utils::miscellaneous::file_path;
 
+pub use pipeline::assistant_config::deprecated::*;
+
 #[cfg(test)]
 mod testutils;
 
