7471eecab0c92e6bd92f97827a3645b41f13a2c4
Adrien Ball <adrien.ball@snips.net>
2017-07-18 15:19:04 +0200

    Remove python test resources

--- queries-embed/python/snips_nlu_rust/tests/utils.py
+++ queries-embed/python/snips_nlu_rust/tests/utils.py
@@ -3,11 +3,12 @@ from __future__ import unicode_literals
 import io
 import os
 
-TEST_PATH = os.path.dirname(os.path.abspath(__file__))
+TEST_DATA_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                              "..", "..", "..", "..", "data", "tests")
 
-SAMPLE_ASSISTANT_DIR = os.path.join(TEST_PATH, "resources", "sample_assistant")
-SAMPLE_ASSISTANT_ZIP_PATH = os.path.join(TEST_PATH, "resources",
-                                         "sample_assistant.zip")
+SAMPLE_ASSISTANT_DIR = os.path.join(TEST_DATA_PATH, "configurations")
+SAMPLE_ASSISTANT_ZIP_PATH = os.path.join(TEST_DATA_PATH, "zip_files",
+                                         "sample_config.zip")
 
 with io.open(SAMPLE_ASSISTANT_ZIP_PATH, mode='rb') as f:
     SAMPLE_ASSISTANT_ZIP = bytearray(f.read())
