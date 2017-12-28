fd748112b14b47be9092aee431389db692567eef
Adrien Ball <adrien.ball@snips.net>
2017-12-28 18:47:40 +0100

    Rename Tagger to SlotFiller and move responsibility from IntentParser to SlotFiller

--- snips-queries/queries-core/src/pipeline/probabilistic/mod.rs
+++ snips-queries/queries-core/src/pipeline/probabilistic/mod.rs
@@ -4,7 +4,7 @@ mod configuration;
 mod features_utils;
 mod intent_classifier;
 mod intent_parser;
-mod tagger;
+mod slot_filler;
 mod feature_processor;
 
 pub use self::configuration::ProbabilisticParserConfiguration;
