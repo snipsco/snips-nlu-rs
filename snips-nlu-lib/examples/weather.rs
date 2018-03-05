361fa27b671135ad6ec220498f3afbb3aff3ca85
Adrien Ball <adrien.ball@snips.net>
2018-03-05 15:58:15 +0100

    Update README and add rust example

---
+++ snips-nlu-lib/examples/weather.rs
@@ -0,0 +1,26 @@
+extern crate snips_nlu_lib;
+extern crate serde_json;
+
+use std::io::prelude::*;
+use std::fs::File;
+use std::env;
+
+use snips_nlu_lib::{SnipsNluEngine, NluEngineConfiguration};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let modelfilename = &args[1];
+    let query = &args[2];
+    let mut file = File::open(modelfilename).expect("Unable to open the file");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).expect("Unable to read the file");
+
+    let configuration: NluEngineConfiguration = serde_json::from_str(&contents).unwrap();
+    let nlu_engine = SnipsNluEngine::new(configuration).unwrap();
+
+    let result = nlu_engine
+        .parse(query, None)
+        .unwrap();
+
+    println!("{}", serde_json::to_string_pretty(&result).unwrap());
+}
