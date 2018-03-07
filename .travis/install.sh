2e05dc199f60b2f3cfa93161cfacc5ddb05a66d7
Adrien Ball <adrien.ball@snips.net>
2018-03-07 13:47:00 +0100

    Add travis CI

---
+++ .travis/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -ev
+
+source .travis/common.sh
+
+echo '
+[workspace]
+members=[
+    "snips-nlu-ffi",
+    "snips-nlu-ffi/python/snips-nlu-python-ffi",
+    "snips-nlu-lib",
+    "snips-nlu-resources",
+    "snips-nlu-resources-packed"
+]' > Cargo.toml
+
+echo "Replacing snips-nlu-ffi url for local version"
+perl -p -i -e "s/^snips-nlu-ffi = .*\$/snips-nlu-ffi = { path = \"..\/..\" \}/g" snips-nlu-ffi/python/snips-nlu-python-ffi/Cargo.toml
