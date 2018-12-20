078df62c9428ce268e2ab11304ce249d84b8df6c
Kevin Lefevre <kevin.lefevre@snips.ai>
2018-12-20 18:39:10 +0100

    Bring back Cargo.toml in root dir

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,13 +3,5 @@ set -ev
 
 source .travis/common.sh
 
-echo '
-[workspace]
-members=[
-    "snips-nlu-ffi",
-    "snips-nlu-ffi/python/snips-nlu-python-ffi",
-    "snips-nlu-lib"
-]' > Cargo.toml
-
 echo "Replacing snips-nlu-ffi url for local version"
 perl -p -i -e "s/^snips-nlu-ffi = .*\$/snips-nlu-ffi = { path = \"..\/..\" \}/g" snips-nlu-ffi/python/snips-nlu-python-ffi/Cargo.toml
