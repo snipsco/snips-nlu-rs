3bf1798636eec93865a58ac41574ea21d346a4d3
Adrien Ball <adrien.ball@snips.ai>
2019-01-07 14:14:42 +0100

    Update the interactive parsing CLI

--- snips-nlu-lib/examples/interactive_parsing_cli.rs
+++ snips-nlu-lib/examples/interactive_parsing_cli.rs
@@ -17,8 +17,17 @@ fn main() {
                 .index(1)
                 .help("path to the trained nlu engine directory"),
         )
+        .arg(
+            Arg::with_name("top_intents")
+                .long("top_intents")
+                .short("t")
+                .required(false)
+                .takes_value(false)
+                .help("option flag to use in order to parse with the `get_intents` API"),
+        )
         .get_matches();
     let engine_dir = matches.value_of("NLU_ENGINE_DIR").unwrap();
+    let top_intents = matches.is_present("top_intents");
 
     println!("\nLoading the nlu engine...");
     let engine = SnipsNluEngine::from_path(engine_dir).unwrap();
@@ -28,8 +37,13 @@ fn main() {
         io::stdout().flush().unwrap();
         let mut query = String::new();
         io::stdin().read_line(&mut query).unwrap();
-        let result = engine.parse(query.trim(), None).unwrap();
-        let result_json = serde_json::to_string_pretty(&result).unwrap();
+        let result_json = if top_intents {
+            let result = engine.get_intents(query.trim()).unwrap();
+            serde_json::to_string_pretty(&result).unwrap()
+        } else {
+            let result = engine.parse(query.trim(), None).unwrap();
+            serde_json::to_string_pretty(&result).unwrap()
+        };
         println!("{}", result_json);
     }
 }
